dc452c9eea0a000d758f7237de1d9388ed874556
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-06-25 22:55:38 +0200

    Update for turn visibilty

--- gameboard.js
+++ gameboard.js
@@ -2,7 +2,7 @@ const gameData = (() => {
     const urlParams = new URLSearchParams(window.location.search);
     const mode = urlParams.get('mode');
     let playerX, playerO, player;
-    
+
     if (mode === 'pvp') {
         playerX = JSON.parse(urlParams.get('playerX'));
         playerO = JSON.parse(urlParams.get('playerO'));
@@ -13,25 +13,25 @@ const gameData = (() => {
         } else {
             // Handle the case when the player parameter is missing
         }
-    }    
-    
-    return { mode, playerX, playerO, player } 
-})() 
+    }
+
+    return { mode, playerX, playerO, player }
+})()
 
 
 const setScoreTableNames = (() => {
     const playerXScoreName = document.getElementById('playerXScoreName')
     const playerOScoreName = document.getElementById('playerOScoreName')
-    if(gameData.mode === 'pvp'){
+    if (gameData.mode === 'pvp') {
         playerXScoreName.textContent = gameData.playerX.name;
         playerOScoreName.textContent = gameData.playerO.name;
     }
-    else{
-        if(gameData.player.mark === 'O'){
+    else {
+        if (gameData.player.mark === 'O') {
             playerXScoreName.textContent = 'Computer';
             playerOScoreName.textContent = gameData.player.name;
         }
-        else{
+        else {
             playerXScoreName.textContent = gameData.player.name;
             playerOScoreName.textContent = 'Computer';
         }
@@ -45,39 +45,39 @@ const gameboardCellHandler = (() => {
 
     cells.forEach((cell, index) => {
         cell.addEventListener('click', (e) => {
-            const target = e.target;  
-            if(target.classList.contains('disabled-cell')) {
-                
-                
+            const target = e.target;
+            if (target.classList.contains('disabled-cell')) {
+
+
             }
-            else if(turn === 'X'){
-                cell.innerHTML='<i class="fa-solid fa-xmark fa-2xl red-mark unclickable"></i>'
+            else if (turn === 'X') {
+                cell.innerHTML = '<i class="fa-solid fa-xmark fa-2xl red-mark unclickable"></i>'
                 target.classList.add('disabled-cell')
                 target.classList.remove('cell-hover')
                 turn = 'O'
 
                 cells.forEach(cell => {
-                    if(cell.classList.contains('disabled-cell')){
+                    if (cell.classList.contains('disabled-cell')) {
 
                     }
-                    else{
+                    else {
                         cell.classList.remove('x-hover')
                         cell.classList.add('o-hover')
                     }
                 })
-                
+
             }
-            else{
-                cell.innerHTML='<i class="fa-solid fa-o fa-2xl blue-mark unclickable"></i>'
+            else {
+                cell.innerHTML = '<i class="fa-solid fa-o fa-2xl blue-mark unclickable"></i>'
                 target.classList.add('disabled-cell')
                 target.classList.remove('cell-hover')
                 turn = 'X'
 
                 cells.forEach(cell => {
-                    if(cell.classList.contains('disabled-cell')){
+                    if (cell.classList.contains('disabled-cell')) {
 
                     }
-                    else{
+                    else {
                         cell.classList.remove('o-hover')
                         cell.classList.add('x-hover')
                     }
@@ -87,6 +87,6 @@ const gameboardCellHandler = (() => {
     })
 
     return { turn, cells }
-})()    
+})()
 
 
